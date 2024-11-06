03e597c30c13d08c3f05b8848f5b128819754654
David Numa <david.numa@gmail.com>
2024-11-06 08:43:41 -0500

    Test created, base page created

---
+++ src/tests/registerNewUser.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@playwright/test";
+import { RegistrationPage } from "../lib/pages/registration.page";
+import { NewUser } from "../lib/interfaces/newUser";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+test.describe(
+  "Register New User",
+  {
+    tag: ["@registerNewUser", "@smoke"],
+  },
+  () => {
+
+    test("Register with email", async ({ page }) => {
+      const registrationPage = new RegistrationPage(page);
+      const userInfo: NewUser = {
+        email: await registrationPage.generateRandomEmail(),
+        password: process.env.PASSWORD || 'Password1!',
+        firstName: process.env.FIRSTNAME || 'David', 
+        lastName: process.env.LASTNAME || 'Numa',
+        phone: await registrationPage.generateRandomPhone(),
+        typeOfAccount: 'Personal',
+        olderThan65: 'No',
+      }
+      
+      await registrationPage.registerNewUser(userInfo);
+      await expect(registrationPage.menuAccount).toBeVisible();
+    });
+
+
+    test.skip("Register with phone", async ({ page }) => {
+      // Todo: Implement test
+    });
+  }
+);
