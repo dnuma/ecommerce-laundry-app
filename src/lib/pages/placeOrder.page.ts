61f37a9f78d64aec01ef142084b7e45ef492bbda
David Numa <david.numa@gmail.com>
2024-11-08 11:48:25 -0500

    Create profile created

--- src/lib/pages/placeOrder.page.ts
+++ src/lib/pages/placeOrder.page.ts
@@ -2,10 +2,17 @@ import type { Page, Locator, FrameLocator } from "@playwright/test";
 import { BasePage } from "./base.page";
 
 export class PlaceOrderPage extends BasePage {
-  readonly page: Page;
+  readonly page: Page;  
   readonly newOrderBtn: Locator;
   readonly startNewOrderBtn: Locator;
   readonly selectProfile: Locator;
+  readonly createNewProfile: Locator;
+  readonly doneBtn: Locator;
+  readonly newProfileName: Locator;
+  readonly pickupLocation: Locator;
+  readonly address: Locator;
+  readonly suggestedAddress: Locator;
+  readonly pickupSpot: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -13,6 +20,15 @@ export class PlaceOrderPage extends BasePage {
     this.newOrderBtn = this.page.locator("#new-order-button");
     this.startNewOrderBtn = this.page.getByText("Start new order");
     this.selectProfile = this.page.getByText("Select Profile Home");
+    this.createNewProfile = this.page.getByRole("radio", {
+      name: "Create a New Profile",
+    });
+    this.doneBtn = this.page.getByRole("button", { name: "Done" });
+    this.newProfileName = this.page.locator("#new-profile-name-input input");
+    this.pickupLocation = this.page.locator("#pickup-location-tag");
+    this.address = this.page.locator("#Line1");
+    this.suggestedAddress = this.page.locator("ul.suggestions-list li").first();
+    this.pickupSpot = this.page.locator("#pickup-location-select");
   }
 
   /**
@@ -28,6 +44,61 @@ export class PlaceOrderPage extends BasePage {
     }
     await this.forcedClick(this.newOrderBtn);
     await this.forcedClick(this.startNewOrderBtn);
+  }
+
+  
+  /**
+   * Creates a user profile by setting a profile name and address, selecting pickup location, and saving the profile.
+   * @param {string} address - Address to associate with the new profile.
+   */
+  async createProfile(address: string) {
+    const continueProfileBtn = this.page.locator(
+      "#choose-profile-continue-button"
+    );
+    const addressSaveBtn = this.page.locator("#address-save-button");
+    const timestamp = new Date().getTime();
+    const profileName = `Home ${timestamp}`;
+
+    await this.forcedClick(this.selectProfile);
+    await this.forcedClick(this.createNewProfile);
+    await this.forcedClick(this.doneBtn);
+
+    await this.newProfileName.fill(profileName);
+    await this.forcedClick(continueProfileBtn);
+
+    await this.forcedClick(this.pickupLocation);
+    await this.address.fill(address);
+    await this.forcedClick(this.suggestedAddress);
+
+    await this.forcedClick(addressSaveBtn);
+    await this.page.waitForSelector(`h2:has-text("Pickup Location")`);
+  }
+
+  /**
+   * Selects a specified pickup spot for the order.
+   * @param {string} spot - The name of the pickup spot.
+   */
+  async selectPickupSpot(spot: string) {
+    const pickupContinueBtn = this.page.locator("#pickup-continue-button");
+    await this.forcedClick(this.pickupSpot);
+    await this.forcedClick(
+      this.page.locator("button:has-text('" + spot + "')")
+    );
+    await this.forcedClick(this.doneBtn);
+    await this.forcedClick(pickupContinueBtn);
+    await this.page.waitForSelector(`h2:has-text("Delivery Speed")`);
+  }
+
+  /**
+   * Selects the delivery speed for the order based on the specified option.
+   * @param {string} speed - The desired delivery speed.
+   */
+  async selectDeliverySpeed(speed: string) {
+    const radio = this.page.getByText(`${speed} Delivery`);
+    const deliveryContinueBtn = this.page.locator("#delivery-continue-button");
+    await this.forcedClick(radio);
+    await this.forcedClick(deliveryContinueBtn);
+    await this.page.waitForSelector(`h2:has-text("Laundry Care")`);
   }
 
 }
--- src/tests/placeNewOrder.spec.ts
+++ src/tests/placeNewOrder.spec.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import { test, expect } from "@playwright/test";
+import { PlaceOrderPage } from "../lib/pages/placeOrder.page";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
@@ -12,6 +13,39 @@ test.describe(
   },
   () => {
     test("New Order", { tag: "@newOrder" }, async ({ page }) => {
+      const placeOrderPage = new PlaceOrderPage(page);
+      const address = "123 Main St";
+      const spot = "Front Door";
+      const deliverySpeed = "Standard";
+      const detergent = "Hypoallergenic";
+      const bagCount = Math.floor(Math.random() * 5) + 1;
+      const typeOfBag = "regular";
+      const oversizedItems = Math.floor(Math.random() * 3) + 1;
+      const coverage = "Premium";
+
+      await test.step("Login", async () => {
+        const email = process.env.EMAIL || "";
+        const password = process.env.PASSWORD || "";
+
+        expect(email).not.toBe("");
+        expect(password).not.toBe("");
+
+        await placeOrderPage.login(email, password);
+        await expect(placeOrderPage.menuAccount).toBeVisible();
+      });
+
+      await test.step("Place New Order", async () => {
+        const title = page.getByRole("heading", { name: "Choose a Profile" });
+        await placeOrderPage.placeNewOrder();
+        await expect(title).toBeVisible();
+      });
+
+      await test.step("Create Profile", async () => {
+        await placeOrderPage.createProfile(address);
+        await placeOrderPage.selectPickupSpot(spot);
+        expect(page.url()).toContain("delivery-speed");
+      });
+
     });
 
   }
